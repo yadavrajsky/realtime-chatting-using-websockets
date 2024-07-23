d32a90fed6a5a5671ff8c8ff7422351fc0743f89
Akash Yadav <yadavrajsky@gmail.com>
2024-07-23 14:19:44 +0530

    Added models to admin control

--- backend/chat/admin.py
+++ backend/chat/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User,Interest,Message
+# Register your models here.
+admin.site.register(User)
+admin.site.register(Interest)
+admin.site.register(Message) 
